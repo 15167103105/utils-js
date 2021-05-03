7696f1e2563544ad3007964541e349899bc24002
贺婷婷 <hetingting@hetingtingdeMacBook-Pro.local>
2021-05-03 16:48:58 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+export { default as formatMoney} from './src/formatMoney';
+export { default as isAvailableTele} from './src/isAvailableTele';
+export { default as isWxWebView} from './src/isWxWebView';
+export { default as mathAdd} from './src/mathAdd';
+export { default as mathDiv} from './src/mathDiv';
+export { default as mathMul} from './src/mathMul';
+export { default as mathSub} from './src/mathSub';
+export { default as randomInt} from './src/randomInt';
