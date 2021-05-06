244285597bb9de0a5079c7def2467a625fa2e833
婷宝 <hetingting@hetingtingdeMacBook-Pro.local>
2021-05-06 21:53:28 +0800

    feat: 优化

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,10 @@ const path = require('path');
 module.exports = {
     mode: 'production', // 因为默认是production 默认会进行压缩
     entry: './src/index.js',
-    externals: ['lodash'],
+    // externals: ['lodash'],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, './dist'),
-        // libraryTarget: 'commonjs2',
         libraryTarget: 'umd',
         library: 'utils-js',
       },
